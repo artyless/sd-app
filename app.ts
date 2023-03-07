58e41184cbec25369a7816705adca2f421bda2fa
Artyom Potykanenko <Drakon_2013_mail@mail.ru>
2023-03-07 04:11:06 +0300

    The project has been set up

--- app.ts
+++ app.ts
@@ -1,6 +1,7 @@
-import express, { Express } from 'express'
+import express, {Express} from 'express'
 import authRoutes from './routes/auth.routes.js'
 import profileRoutes from './routes/profile.routes.js'
+import imageRoutes from './routes/image.routes.js'
 
 import dotenv from 'dotenv'
 
@@ -9,21 +10,24 @@ const PORT = process.env.SERVER_PORT
 
 const app: Express = express()
 
-app.use(express.json())
+app.use(express.json({limit: '5mb'}))
+// app.use(express.urlencoded({limit: '5mb', extended: true}))
 
 app.use('/api/auth', authRoutes)
 app.use('/api/profile', profileRoutes)
+app.use('/api/image', imageRoutes)
 
 const start = async () => {
-  try {
-    app.listen(PORT, () => {
-      console.log(`[server]: Server is running at http://localhost:${PORT}`)
-    })
-  } catch (e: any) {
-    console.log('Server Error: ', e.message)
-    process.exit(1)
-  }
+    try {
+        app.listen(PORT, () => {
+            console.log(`[server]: Server is running at http://localhost:${PORT}`)
+        })
+    } catch (e: any) {
+        console.log('Server Error: ', e.message)
+        process.exit(1)
+    }
 }
 
-start().then()
-
+start().catch((e: Error) => {
+    console.log('Error. Server was stopped: ', e)
+})
